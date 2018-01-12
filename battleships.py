75948955d7f49ef2f4a9517d158420a2afb9cba2
Elin Falla <elinfalla@Elin-Fallas-MacBook-2.local>
2018-01-12 02:13:03 +0000

    combined assessHit and assessCoordinate and added main function

--- battleships.py
+++ battleships.py
@@ -24,36 +24,36 @@ def gen_ship(board):  #brackets = parameters, not necesarily same 'board'as belo
 def gen_board():  #colon needed when line of code creates new indent
     board = []  # [] means an empty list
     for x in range(0, GRID_SIZE_X):
-        '''
-        Don't need to initialise x here, x starts as 0, dont have to specify
-        type in python, knows range returns list of integers.
-        '''
+
+        #Don't need to initialise x here, x starts as 0, dont have to specify
+        #type in python, knows range returns list of integers.
+
         board.append([])
-        '''
-        append means add on to end, so for every x value you add a new empty
-        list, hence making it 2 dimensional.
-        '''
+
+        #append means add on to end, so for every x value you add a new empty
+        #list, hence making it 2 dimensional.
+
         for y in range(0, GRID_SIZE_Y):
-            '''
-            Put y for loop in x one (NESTED FOR LOOP) so you get a y value
-            for every x value not just a y axis of values - means loop does x=0
-            then y=0 up to 10 then does for x=1, y=0 to 10.
-            '''
+
+            #Put y for loop in x one (NESTED FOR LOOP) so you get a y value
+            #for every x value not just a y axis of values - means loop does x=0
+            #then y=0 up to 10 then does for x=1, y=0 to 10.
+
             board[x].append(0)
 
     for d in range(0, NUM_SHIPS):
-        '''
-        ew - can do it like below and not as a recursive function
-        (function that calls itself) but is less readable and easy to understand
-        --> needed to ensure the same square isnt randomly generated more than once
 
-        x = random.randint(0,GRID_SIZE_X - 1)
-        y = random.randint(0,GRID_SIZE_Y - 1)
-        while board[x][y] == 1:
-            x = random.randint(0,GRID_SIZE_X - 1)
-            y = random.randint(0,GRID_SIZE_X - 1)
-        board[x][y] = 1
-        '''
+        #can do it like below and not as a recursive function
+        #(function that calls itself) but is less readable and easy to understand
+        #--> needed to ensure the same square isnt randomly generated more than once
+
+        #x = random.randint(0,GRID_SIZE_X - 1)
+        #y = random.randint(0,GRID_SIZE_Y - 1)
+        #while board[x][y] == 1:
+        #    x = random.randint(0,GRID_SIZE_X - 1)
+        #    y = random.randint(0,GRID_SIZE_X - 1)
+        #board[x][y] = 1
+
         gen_ship(board)
 
     return board
@@ -79,13 +79,14 @@ def print_board(board):
         print_line()
 
 
-def assessHit(board,x,y):
+'''def assessHit(board,x,y):
 
     if board[x][y] == 1:
         board[x][y] = str('!')
         print_board(board)
         print("Hit!")
-        return True
+        numberHit += 1
+        return numberHit
 
     elif board[x][y] == 0:
         board[x][y] = str('x')
@@ -93,7 +94,7 @@ def assessHit(board,x,y):
         print("Miss!")
 
     else:
-        print("Coordinate already tried")
+        print("Coordinate already tried")'''
 
 def enterCoordinates():
         attempt = input("Enter hit coordinates: ")
@@ -103,33 +104,52 @@ def assessCoordinate(board):
     coords = enterCoordinates().split(",") #splits the inputted string (coordinate) into a list defined by the comma
     if len(coords) == 2: #len=length ie. how many values in list
         print("Correct length coordinate")
+
+        if int(coords[0]) in range(1, GRID_SIZE_X + 1) and int(coords[1]) in range(1, GRID_SIZE_Y + 1):
+# range includes first number given (1) but not last number given (grid size + 1)
+
+                x = int(coords[0]) - 1 #to make coordinates 1-10 not 0-9
+                y = int(coords[1]) - 1
+                if board[x][y] == 1:
+                    board[x][y] = str('!')
+                    print_board(board)
+                    print("Hit!")
+                    numberHit += 1
+                    return numberHit
+
+                elif board[x][y] == 0:
+                    board[x][y] = str('x')
+                    print_board(board)
+                    print("Miss!")
+                    #return numberHit
+
+                else:
+                    print("Coordinate already tried")
+                #if assessHit(board,x,y) == True: #this calls it as well as being a conditional so will still print hit/miss etc
+                    #return True
+
+        else:
+            print("Coordinates must be between 1 and ", GRID_SIZE_X, " - try again")
     else:
-        print("Coordinate must be two numbers separated by comma")
-
-    if int(coords[0]) in range(0, GRID_SIZE_X + 1) and int(coords[1]) in range(0, GRID_SIZE_Y + 1):  #TODO
-
-        x = int(coords[0]) - 1 #to make coordinates 1-10 not 0-9
-        y = int(coords[1]) - 1
-        if assessHit(board,x,y) == 1: #this calls it as well as being a conditional so will still print hit/miss etc
-            return True
-    else:
-        print("Not a coordinate - try again")
+        print("Coordinate must be two numbers separated by comma - try again")
 
 
+def main():
+    board = gen_board() #generates a board
+    print_board(board) #prints that board
+    '''
+    def shipsLeft():
+    count = NUM_SHIPS
+    for count in range(NUM_SHIPS, 0, -1):
+        if assessCoordinate(board) == True:
+            count -= 1 #count = count - 1
+    return count'''
 
-board = gen_board() #generates a board
-print_board(board) #prints that board
+    while assessCoordinate(board) < NUM_SHIPS:
+        enterCoordinates()
 
-#def shipsLeft():
-#    count = NUM_SHIPS
-#    for count in range(NUM_SHIPS, 0, -1):
-#        if assessCoordinate(board) == True:
-#            count -= 1 #count = count - 1
-#    return count
+        assessCoordinate(board)
 
-while shipsLeft() > 0:
-    enterCoordinates()
+    print("You've won!")
 
-    assessCoordinate(board)
-
-print("You've won!")
+main()
